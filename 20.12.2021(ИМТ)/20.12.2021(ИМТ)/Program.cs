84c7da65b5f2f51eca8f1db345a877c504dd3f0a
justify1852 <96074966+justify1852@users.noreply.github.com>
2021-12-24 09:29:00 +0300

    Add files via upload

---
+++ 20.12.2021(ИМТ)/20.12.2021(ИМТ)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            Console.Write("Введите ваш вес(кг): ");
+            double weight = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите ваш рост(м): ");
+            double height = Convert.ToDouble(Console.ReadLine());
+            double IMT = weight / (height * 2);
+            Console.WriteLine($"Ваш индекс массы тела равен: {IMT}");
+        }
+
+    }
+}
