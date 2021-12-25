1ac483ac51fa8b82903fb67721c470393451d8a5
justify1852 <96074966+justify1852@users.noreply.github.com>
2021-12-25 20:24:34 +0300

    Add files via upload

---
+++ 20.12.2021(calc +)/20.12.2021(calc +)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace project
+{
+
+    class Program
+    {
+
+        static void Main()
+        {
+            int num_1, num_2, equals;
+
+            Console.Write("Это калькулятор, но он пока что умеет только складывать два числа, и то только целые числа.\nВведите первое число: ");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            num_2 = Convert.ToInt32(Console.ReadLine());
+            equals = num_1 + num_2;
+            Console.WriteLine($"{num_1} + {num_2} равно {equals}");
+
+        }
+
+    }
+
+}
