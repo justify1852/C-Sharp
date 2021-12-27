ca340e5a3f967c444033295b1b1dfac1f7cde073
justify1852 <96074966+justify1852@users.noreply.github.com>
2021-12-27 10:03:04 +0300

    Add files via upload

---
+++ 23.12.2021(цикл числа)/23.12.2021(цикл числа)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int i = 1; i <= 20; i++)
+            {
+                if (i % 2 != 0)
+
+                    continue;
+
+                Console.WriteLine($"четное число {i}");
+            }
+            Console.WriteLine();
+            for (int y = 1; y <= 20; y++)
+            {
+                if (y % 2 == 0)
+
+                    continue;
+
+                Console.WriteLine($"нечетное число {y} ");
+            }
+
+        }
+    }
+}
