b49488b6328c53d22fb5b4f1beb4f3bcbd6efbcb
justify1852 <96074966+justify1852@users.noreply.github.com>
2021-12-29 06:09:41 +0300

    Add files via upload

---
+++ 24.12.2021(сортировка чисел)/24.12.2021(сортировка чисел)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<double> numbers = new List<double>()
+            {
+                23.3,1.8,2.5,1.87,
+            };
+
+            numbers.Sort();
+
+            foreach(double i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
