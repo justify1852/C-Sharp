be1ceeed412c408916a320a65f49c1a96dd6bff0
justify1852 <96074966+justify1852@users.noreply.github.com>
2021-12-30 11:42:01 +0300

    Add files via upload

---
+++ 24.12.2021(использование метода)/24.12.2021(использование метода)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte[] nums = { 5, 7, 8, 3, 5 };
+            byte res1 = Summa(nums);
+            Console.WriteLine("Результат:" + res1);
+
+            byte[] nums1 = { 5, 3, 8, 3, 5 };
+            byte res2 = Summa(nums1);
+            Console.WriteLine("Результат:" + res2);
+        }
+
+        public static byte Summa(byte[] digits)
+        {
+            byte summ = 0;
+            foreach (byte el in digits)
+                summ += el;
+
+            return summ;
+        }
+
+    }
+}
