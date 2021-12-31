2f32a968934f42c9fc4c525b20e52e71ae57612b
justify1852 <96074966+justify1852@users.noreply.github.com>
2021-12-31 13:52:30 +0300

    Add files via upload

---
+++ 24.12.2021(возраст)/24.12.2021(возраст)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("напиши год рождения и я скажу возраст");
+
+            int dateOfBorn = Convert.ToInt32(Console.ReadLine());
+            string currentDate = DateTime.Now.ToString("yyyy-MM-dd");
+            string year = currentDate.Substring(0, 4);
+            int convertYear = Convert.ToInt32(year);
+            int unswer = convertYear - dateOfBorn;
+
+            Console.WriteLine("возраст " + unswer + " лет");
+        }
+    }
+}
