2d631dd33da927720f699b160de3297e190f2c17
justify1852 <96074966+justify1852@users.noreply.github.com>
2022-01-03 19:19:11 +0300

    Add files via upload

---
+++ 03.01.2022(проверка на четность)/03.01.2022(проверка на четность)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Введите число ");
+
+            double number = Convert.ToDouble(Console.ReadLine());
+
+            if(number % 2 != 0)
+            {
+                Console.WriteLine("это нечетное число");
+            }
+            else
+            {
+                Console.WriteLine("это четное число");
+            }
+        }
+    }
+}
